da11ed98ab445ed6389b6cf5a2d50caa91de5ae0
Jeff <jeff.uca@gmail.com>
2020-03-18 19:25:02 +0100

    :sparkles: add chart plotting in real time

--- RealTimeChart.java
+++ RealTimeChart.java
@@ -17,56 +17,76 @@ public class RealTimeChart {
     public XYChart chart;
     public JFrame chart_frame;
 
-
     public void show(){
         // Create Chart
-        chart = QuickChart.getChart("Generic Chart", "X", "Y", "randomWalk",
+        chart = QuickChart.getChart("Generic Chart", "Generations", "Cells Number", "randomWalk",
                 new double[] { 0 }, new double[] { 0 });
-        chart.getStyler().setLegendVisible(false);
-        chart.getStyler().setXAxisTicksVisible(false);
-
+        chart.addSeries("corona",new double[] { 0 }, new double[] { 0 });
+        chart.getStyler().setLegendVisible(true);
+        chart.getStyler().setXAxisTicksVisible(true);
         // Show it
         sw = new SwingWrapper<XYChart>(chart);
         chart_frame = sw.displayChart();
         chart_frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-        chart_frame.setTitle("Generic Char");
+        chart_frame.setTitle("Generic Chart");
         chart_frame.validate();
         chart_frame.repaint();
 
 
     }
 
-    public void go() {
+    public void go(int generations, int states) {
 
-
-        mySwingWorker = new MySwingWorker();
+        mySwingWorker = new MySwingWorker(generations,states);
         mySwingWorker.execute();
     }
 
     private class MySwingWorker extends SwingWorker<Boolean, double[]> {
 
-        LinkedList<Double> fifo = new LinkedList<Double>();
+        private LinkedList<Integer>[] fifo;
+        private int generations;
+        private int states;
 
-        public MySwingWorker() {
-
-            fifo.add(0.0);
+        public MySwingWorker(int generations, int states) {
+            this.generations = generations;
+            this.states= states;
+            fifo = new LinkedList[states];
+            for (int i = 0; i < states ; i++) {
+                fifo[i] = new LinkedList<Integer>();
+            }
+            fifo[0].add(0);
         }
 
         @Override
         protected Boolean doInBackground() throws Exception {
 
-            while (!isCancelled()) {
+            int point = 0;
 
-                fifo.add(fifo.get(fifo.size() - 1) + Math.random() - .5);
-                if (fifo.size() > 500) {
-                    fifo.removeFirst();
-                }
+            while (point < generations) {
 
-                double[] array = new double[fifo.size()];
-                for (int i = 0; i < fifo.size(); i++) {
-                    array[i] = fifo.get(i);
+                point++;
+
+                fifo[0].add(point);
+
+                double[] array = new double[fifo[0].size()];
+                double[] array1 = new double[fifo[0].size()];
+                double [] datax = new double[fifo[0].size()];
+                for (int i = 0; i < fifo[0].size(); i++) {
+                    array[i] = fifo[0].get(i);
+                    array1[i] = fifo[0].get(i)*4;
+                    datax[i] = i;
                 }
-                publish(array);
+                chart.updateXYSeries("randomWalk", datax, array, null);
+                chart.updateXYSeries("corona", datax, array1, null);
+                sw.repaintChart();
+
+                long start = System.currentTimeMillis();
+                long duration = System.currentTimeMillis() - start;
+                try {
+                    Thread.sleep(40 - duration); // 40 ms ==> 25fps
+                    // Thread.sleep(400 - duration); // 40 ms ==> 2.5fps
+                } catch (InterruptedException e) {
+                }
 
                 try {
                     Thread.sleep(5);
@@ -80,23 +100,5 @@ public class RealTimeChart {
             return true;
         }
 
-        @Override
-        protected void process(List<double[]> chunks) {
-
-            System.out.println("number of chunks: " + chunks.size());
-
-            double[] mostRecentDataSet = chunks.get(chunks.size() - 1);
-            chart.updateXYSeries("randomWalk", null, mostRecentDataSet, null);
-            sw.repaintChart();
-
-            long start = System.currentTimeMillis();
-            long duration = System.currentTimeMillis() - start;
-            try {
-                Thread.sleep(40 - duration); // 40 ms ==> 25fps
-                // Thread.sleep(400 - duration); // 40 ms ==> 2.5fps
-                 } catch (InterruptedException e) {
-            }
-
-        }
     }
 }
