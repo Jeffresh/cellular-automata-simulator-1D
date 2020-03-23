e3d22157bde003e2345d5b7ba76c834a019eadf1
Jeff <jeff.uca@gmail.com>
2020-03-23 23:01:16 +0100

    :recycled: unused variables, import, spaces and comments

--- PopulationChart.java
+++ PopulationChart.java
@@ -1,8 +1,5 @@
 import java.awt.*;
 import java.util.LinkedList;
-import java.util.List;
-
-import org.knowm.xchart.QuickChart;
 import org.knowm.xchart.XChartPanel;
 import org.knowm.xchart.XYChart;
 import javax.swing.*;
@@ -14,8 +11,6 @@ import org.knowm.xchart.*;
  */
 public class PopulationChart {
 
-    public SwingWorker mySwingWorker;
-//    public static SwingWrapper<XYChart> sw;
     public static JPanel sw;
     public static XYChart chart;
     public static JFrame chart_frame;
@@ -40,7 +35,6 @@ public class PopulationChart {
     public void getData () {
 
         fifo = CA1Dref.getPopulation();
-        double [] datax = new double[CA1Dref.generations];
         double[][] array = new double[CA1Dref.states_number][fifo[0].size()];
 
         for (int j = 0; j < CA1Dref.states_number; j++) {
@@ -50,8 +44,6 @@ public class PopulationChart {
             }
             chart.updateXYSeries("state "+(j),null, array[j], null);
         }
-
-
 
     }
 
@@ -67,11 +59,8 @@ public class PopulationChart {
         getData();
         sw.revalidate();
         sw.repaint();
-
     }
-
     public void show(){
-
 
             sw = new XChartPanel(chart);
             chart_frame = new JFrame("chart");
@@ -84,78 +73,5 @@ public class PopulationChart {
             chart_frame.setVisible(true);
             chart_frame.pack();
 
-
-
-
     }
-
-    //TODO PAINT IN REAL TIME
-//    public void go(int generations, int states) {
-//        mySwingWorker = new MySwingWorker(generations,states);
-//        mySwingWorker.execute();
-//    }
-
-//    private class MySwingWorker extends SwingWorker<Boolean, double[]> {
-//
-//        private LinkedList<Integer>[] fifo;
-//        private int generations;
-//        private int states;
-//        private double [] array;
-//
-//        public MySwingWorker(int generations, int states) {
-//            this.generations = generations;
-//            this.states= states;
-//            for (int i = 0; i < states ; i++) {
-//                if(i>0)
-//                    chart.addSeries("state "+(i+1),new double[] { 0 }, new double[] { 0 });
-//            }
-//
-//            array = new double[generations];
-//        }
-//
-//        @Override
-//        protected Boolean doInBackground() throws Exception {
-//
-//            int point = 0;
-//
-//            while (point < generations) {
-//
-//                point++;
-//                fifo = CA1Dref.nextGen(point);
-//
-//                double [] datax = new double[point];
-//                double[][] array = new double[2][point];
-//
-//                for (int j = 0; j < states; j++) {
-//                    for (int i = 0; i < fifo[j].size(); i++) {
-//
-//                        array[j][i] = fifo[j].get(i);
-//                        datax[i] = i;
-//                    }
-//                    chart.updateXYSeries("state "+(j+1), datax, array[j], null);
-//                }
-//                sw.repaintChart();
-//                CA1Dref.canvasTemplateRef.repaint(1000,1000,1000,1000);
-//
-//                long start = System.currentTimeMillis();
-//                long duration = System.currentTimeMillis() - start;
-//                try {
-//                    Thread.sleep(40 - duration); // 40 ms ==> 25fps
-//                    // Thread.sleep(400 - duration); // 40 ms ==> 2.5fps
-//                } catch (InterruptedException e) {
-//                }
-//
-//                try {
-//                    Thread.sleep(5);
-//                } catch (InterruptedException e) {
-//                    // eat it. caught when interrupt is called
-//                    System.out.println("MySwingWorker shut down.");
-//                }
-//
-//            }
-//
-//            return true;
-//        }
-//
-//    }
 }
